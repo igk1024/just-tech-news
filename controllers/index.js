0a470bcca8984aa0b6810b35cef31a4b74286975
Ida <67982739+igk1024@users.noreply.github.com>
2020-10-30 14:35:43 -0500

    updated controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,12 @@
 const router = require('express').Router();
-
+const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
 });
-
-const homeRoutes = require('./home-routes.js');
-
-router.use('/', homeRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// turn on routes
-app.use(routes);
 
 //stylesheet in public folder
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static('public'));
 
 //set up handlebars as template engine
 const exphbs = require('express-handlebars');
@@ -23,6 +23,9 @@ const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+// turn on routes
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
